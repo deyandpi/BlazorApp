2d1b3698f15354b00c342126a5f7e24d5873589d
deyan ivanov <deyan.p.ivanov@smartit.bg>
2019-11-15 08:40:46 +0200

    Add project files.

---
+++ Data/EventsService.cs
@@ -0,0 +1,30 @@
+﻿using DbData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazerTest1.Data
+{
+    public class EventsService
+    {
+        public Task<List<EventDb>> GetEvents(LocalDBContext context)
+        {
+            //LocalDBContext context = new LocalDBContext();
+            return Task.FromResult(context.Events.Select(s => new EventDb { Name = s.Name, Date = s.Date }).ToList());
+        }
+
+        public bool Save(EventDb context)
+        {
+            //LocalDBContext context = new LocalDBContext();
+            return true;
+        }
+    }
+
+    public class EventDb
+    {
+        public string Name { get; set; }
+
+        public DateTime Date{ get; set; }
+    }
+}
---
+++ Readme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazerTest1
+{
+    public class Readme
+    {
+        /*
+         Unable to find provider assembly with name Microsoft.EntityFrameworkCore.SqlServer
+         Install-Package Microsoft.EntityFrameworkCore.SqlServer
+         */
+    }
+}
